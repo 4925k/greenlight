96ec889ef51337303483b1737952de43f19a8c49
dibek <dibekpoudyal@gmail.com>
2023-11-14 21:33:41 +0545

    add protection against data races while updating

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -46,7 +46,17 @@ func (m MovieModel) Insert(movie *Movie) error {
 
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
@@ -81,9 +91,9 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 func (m MovieModel) Update(movie *Movie) error {
 	query := `UPDATE movies SET title = $1, year = $2, runtime = $3, genres = $4, version = version +1
-				WHERE id = $5 RETURNING version`
+				WHERE id = $5 AND version = $6 RETURNING version`
 
-	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID}
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres), movie.ID, movie.Version}
 
 	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
