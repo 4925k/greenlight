dd38d77f696c1acea1abc59b0a965f86f45a339f
dibek <dibekpoudyal@gmail.com>
2023-11-16 17:14:21 +0545

    added support for sorting

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "github.com/4925k/greenlight/internal/validator"
+import (
+	"github.com/4925k/greenlight/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -17,4 +20,24 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be less than 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	// the Sort value should have already been checked by calling theValidateFilters() function —
+	// but this is a sensible failsafe to help stop a SQL injection attack occurring
+	panic("unsafe sort parameter" + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
