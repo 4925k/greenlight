c522883cba4494ee94dbd3902ff82047864f2320
dibek <dibekpoudyal@gmail.com>
2023-11-21 18:33:39 +0545

    added endpoint to register users

--- internal/data/users.go
+++ internal/data/users.go
@@ -14,12 +14,12 @@ var (
 )
 
 type User struct {
-	ID        int64     `json:"id,omitempty"`
+	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
-	Name      string    `json:"name,omitempty"`
-	Email     string    `json:"email,omitempty"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
 	Password  password  `json:"-"`
-	Activated bool      `json:"activated,omitempty"`
+	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
 }
 
