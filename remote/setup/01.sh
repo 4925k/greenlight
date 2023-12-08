7a8deb6ac48162ffef22d6a4a4f3063def95afa1
dibek <dibekpoudyal@gmail.com>
2023-12-08 23:42:16 +0545

    bash script for setting up servers

---
+++ remote/setup/01.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -eu
+
+## VARIABLES
+
+# time zone
+TIMEZONE=America/New_York
+
+# username
+USERNAME=greenlight
+
+# get database password
+read -p "Enter password for greenlight DB user:" DB_PASSWORD
+
+# set locale to english
+export LC_ALL=en_US.UTF-8
+
+## SCRIPT
+
+# enable the universe repo
+add-apt-repository --yes universe
+
+# update all software packages
+apt update
+apt --yes -o Dpkg::Options::="--force-confnew" upgrade
+
+# set system timezone and install locales
+timedatectl set-timezone ${TIMEZONE}
+apt --yes install locales-all
+
+# add new user with sudo privileges
+useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
+
+# force password reset
+passwd --delete "${USERNAME}"
+chage --lastday 0 "${USERNAME}"
+
+# configure firewall to allow SSH, HTTP and HTTPS traffic
+ufw allow 22
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw --force enable
+
+# install fail2ban
+apt --yes install fail2ban
+
+# install migrate tools
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvzmv migrate.linux-amd64 /usr/local/bin/migrate
+
+# install postgres
+apt --yes install postgresql
+
+# set up greenlight DB and create user with the password entered earlier
+sudo -i -u postgres psql -c "CREATE DATABASE greenlight"
+sudo -i -u postgres psql -d greenlight -c "CREATE EXTENSION IF NOT EXISTS citext"
+sudo -i -u postgres psql -d greenlight -c "CREATE ROLE greenlight WITH LOGIN PASSWORD '${DB_PASSWORD}'"
+
+# add database dsn to environment variable
+echo "GREENLIGHT_DB_DSN='postgres://greenlight:${DB_PASSWORD}@localhost/greenlight?sslmode=disable"
+
+# install caddy
+apt --yes install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -L https://dl.cloudsmith.io/public/caddy/stable/gpg.key | sudo apt-key add -
+curl -L https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+apt update
+apt --yes install caddy
+
+echo "Script complete! Rebooting"
+reboot
